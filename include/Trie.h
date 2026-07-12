55426a713692f08421ad6ac9117a601565c5b0c7
DILESH CHURIWAL <dileshchuriwal2004@gmail.com>
2026-07-12 10:42:57 +0530

    Add Trie class interface

--- include/Trie.h
+++ include/Trie.h
@@ -1,1 +1,35 @@
+#ifndef TRIE_H
+#define TRIE_H
 
+#include <string>
+#include <vector>
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode();
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+    void dfs(TrieNode* node, string currentWord,
+             vector<string>& suggestions);
+
+public:
+    Trie();
+
+    void insert(const string& word);
+
+    bool search(const string& word);
+
+    bool startsWith(const string& prefix);
+
+    vector<string> autocomplete(const string& prefix);
+};
+
+#endif
