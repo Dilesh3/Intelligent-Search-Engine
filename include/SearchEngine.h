d2e11290f9d64ee37d2d60099ceb9b97cc590486
DILESH CHURIWAL <dileshchuriwal2004@gmail.com>
2026-07-12 10:44:29 +0530

    Add SearchEngine Interface

--- include/SearchEngine.h
+++ include/SearchEngine.h
@@ -1,1 +1,22 @@
+#ifndef SEARCHENGINE_H
+#define SEARCHENGINE_H
 
+#include "Trie.h"
+
+class SearchEngine {
+
+private:
+
+    Trie trie;
+
+public:
+
+    SearchEngine();
+
+    void buildIndex();
+
+    void search();
+
+};
+
+#endif
