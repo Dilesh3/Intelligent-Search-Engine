93977d7fe7b04832b4d5c22968975fe931604116
DILESH CHURIWAL <dileshchuriwal2004@gmail.com>
2026-07-12 10:43:43 +0530

    Initialize Trie Implementation

--- src/Trie.cpp
+++ src/Trie.cpp
@@ -1,1 +1,34 @@
+#include "../include/Trie.h"
 
+TrieNode::TrieNode() {
+    isEnd = false;
+    for(int i=0;i<26;i++)
+        children[i]=nullptr;
+}
+
+Trie::Trie() {
+    root = new TrieNode();
+}
+
+void Trie::insert(const string& word) {
+
+}
+
+bool Trie::search(const string& word) {
+    return false;
+}
+
+bool Trie::startsWith(const string& prefix) {
+    return false;
+}
+
+void Trie::dfs(TrieNode* node,
+               string currentWord,
+               vector<string>& suggestions) {
+
+}
+
+vector<string> Trie::autocomplete(const string& prefix) {
+    vector<string> result;
+    return result;
+}
