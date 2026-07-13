de7a5b61e9d0e3fe2ed39f4ef3f31c6b172e3e5d
DILESH CHURIWAL <dileshchuriwal2004@gmail.com>
2026-07-13 11:10:06 +0530

    Implemented Trie functions except the optimized functions

--- src/Trie.cpp
+++ src/Trie.cpp
@@ -1,34 +1,124 @@
 #include "../include/Trie.h"
 
 TrieNode::TrieNode() {
-    isEnd = false;
+    isEndOfWord = false;
     for(int i=0;i<26;i++)
         children[i]=nullptr;
+    frequency = 0;
 }
 
 Trie::Trie() {
     root = new TrieNode();
 }
 
-void Trie::insert(const string& word) {
-
+Trie::~Trie() {
+    deleteTrie(root);
 }
 
-bool Trie::search(const string& word) {
+
+void Trie::deleteTrie(TrieNode* node) {
+    if(node == nullptr) return;
+    for(int i = 0; i < 26; i++) {
+        deleteTrie(node->children[i]);
+    }
+    delete node;
+}
+
+
+void Trie::insert(const std::string& word) {
+    TrieNode* current = root;
+
+    for (char ch : word) {
+        int index = ch - 'a';
+
+        if (current->children[index] == nullptr) {
+            current->children[index] = new TrieNode();
+        }
+
+        current = current->children[index];
+    }
+
+        if (!current->isEndOfWord) {
+            current->isEndOfWord = true;
+            current->frequency = 0;
+        }
+}
+
+
+TrieNode* Trie::getNode(const std::string &prefix) const{
+    TrieNode* current = root;
+    for(char ch : prefix) {
+        int index = ch - 'a';
+
+        if(current->children[index] == nullptr) {
+            return nullptr;
+        }
+        current = current->children[index];
+    }
+    return current;
+}
+
+
+bool Trie::search(const std::string& word) const{
+    TrieNode* node = getNode(word);
+    if(node == nullptr) return false;
+    return node->isEndOfWord;
+}
+
+
+bool Trie::startsWith(const std::string& prefix) const{
+    TrieNode* node = getNode(prefix);
+    if(node != nullptr) return true;
     return false;
 }
 
-bool Trie::startsWith(const string& prefix) {
-    return false;
+
+void Trie::dfs(TrieNode* node, std::string& currentWord,
+               std::vector<std::pair<std::string, int>>& suggestions) const {
+
+    if (node == nullptr)  return;
+
+    if (node->isEndOfWord) {
+        suggestions.emplace_back(currentWord, node->frequency);
+    }
+
+    for (int i = 0; i < 26; i++) {
+
+        if (node->children[i] != nullptr) {
+
+            currentWord.emplace_back('a' + i);
+
+            dfs(node->children[i], currentWord, suggestions);
+
+            currentWord.pop_back();
+        }
+    }
 }
 
-void Trie::dfs(TrieNode* node,
-               string currentWord,
-               vector<string>& suggestions) {
 
+std::vector<std::pair<std::string, int>> Trie::autocomplete(const std::string& prefix) const {
+    std::vector<std::pair<std::string, int>> suggestions;
+
+    TrieNode* node = getNode(prefix);
+
+    if (node == nullptr)  return suggestions;
+
+    std::string currentWord = prefix;
+
+    dfs(node, currentWord, suggestions);
+
+    std::sort(suggestions.begin(), suggestions.end(),
+        [](const auto& a, const auto& b) {
+            return a.second > b.second;
+        });
+
+    return suggestions;
 }
 
-vector<string> Trie::autocomplete(const string& prefix) {
-    vector<string> result;
-    return result;
+void Trie::updateFrequency(const std::string& word) {
+    TrieNode* node = getNode(word);
+
+    if (node != nullptr && node->isEndOfWord) {
+        node->frequency++;
+    }
 }
