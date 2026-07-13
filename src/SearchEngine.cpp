b96912b37ff7a6fa3e05f60c256a8f1da37b850c
DILESH CHURIWAL <dileshchuriwal2004@gmail.com>
2026-07-13 14:04:54 +0530

    Incorporated SpellCorrector
    Refactor loadDictionary to load both trie and spell corrector dictionaries. Update search method to include frequency update. Add autocomplete and spellCorrect methods.

--- src/SearchEngine.cpp
+++ src/SearchEngine.cpp
@@ -1,20 +1,38 @@
 #include "../include/SearchEngine.h"
 
 bool SearchEngine::loadDictionary(const std::string& filename) {
-    return DictionaryLoader::loadDictionary(filename, trie);
+
+    bool trieLoaded =
+        DictionaryLoader::loadDictionary(filename, trie);
+
+    bool spellLoaded =
+        spellCorrector.loadDictionary(filename);
+
+    return trieLoaded && spellLoaded;
 }
 
 bool SearchEngine::search(const std::string& word) {
 
     if (trie.search(word)) {
+
         trie.updateFrequency(word);
+
         return true;
     }
 
     return false;
 }
 
-std::vector<std::pair<std::string, int>>
+std::vector<std::pair<std::string,int>>
 SearchEngine::autocomplete(const std::string& prefix) const {
+
     return trie.autocomplete(prefix);
+}
+
+std::vector<std::pair<std::string,int>>
+SearchEngine::spellCorrect(
+    const std::string& word,
+    int maxSuggestions) const {
+
+    return spellCorrector.suggest(word, maxSuggestions);
 }
